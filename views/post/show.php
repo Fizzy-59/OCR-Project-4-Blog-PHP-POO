026c2dced40368fe288c4f703087f4c79f9a7881
Romain <rom.dev@live.fr>
2020-04-18 15:40:52 +0200

    Page Category complete

--- views/post/show.php
+++ views/post/show.php
@@ -12,9 +12,7 @@ $pdo = Connection::getPDO();
 // Post
 $query = $pdo->prepare('SELECT * FROM post WHERE id = :id');
 $query->execute(['id' => $id]);
-
 $query->setFetchMode(PDO::FETCH_CLASS, Post::class);
-
 /** @var Post|false */
 $post = $query->fetch();
 
