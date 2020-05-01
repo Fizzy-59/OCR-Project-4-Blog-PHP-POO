d24410e7ca7ec58c1a1b28002eb580ffd0346302
Romain <rom.dev@live.fr>
2020-05-01 18:05:05 +0200

    Comment for update develop

--- src/Table/Exception/NotFoundException.php
+++ src/Table/Exception/NotFoundException.php
@@ -8,14 +8,15 @@ use Exception;
 
 class NotFoundException extends Exception
 {
-    /**
-     * NotFoundException constructor.
-     *
-     * @param string $table
-     * @param $id
-     */
-    public function __construct(string $table, $id)
-    {
-        $this->message = "Aucun enregistrement ne correspond à l'id #$id dans la table '$table'";
-    }
+//    /**
+//     * NotFoundException constructor.
+//     *
+//     * @param string $table
+//     * @param $id
+//     */
+//    public function __construct(string $table, $id)
+//    {
+//
+//        $this->message = "Aucun enregistrement ne correspond à l'id #$id dans la table '$table'";
+//    }
 }
